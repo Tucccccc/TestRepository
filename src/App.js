624f007fd2256deeeb34d3a833da5e5bdd62246f
Tucccccc <singungocv11@gmail.com>
2022-09-15 16:21:55 +0700

    modified:   src/App.js
     Untracked files:
    	src/style.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="form">
+    <form>
+      <div className="input-container">
+        <label>Username </label>
+        <input type="text" name="uname" required />
+      </div>
+      <div className="input-container">
+        <label>Password </label>
+        <input type="password" name="pass" required />
+      </div>
+      <div className="button-container">
+        <input type="submit" />
+      </div>
+    </form>
+  </div>
   );
 }
 
